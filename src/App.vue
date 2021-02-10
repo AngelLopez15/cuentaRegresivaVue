b0e6f7e41b888f3d91450f5eaf450010346f8132
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2021-02-09 22:27:44 -0600

    Estilos agregados

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,17 @@ export default {
 </script>
 
 <style>
-#app {
-  
+:root {
+  --primary-color: #474d8a;
+  --secondary-color: #3e99d5;
+  --support-color: #1e3c91;
+  --action-color: #ef7a23;
+  --alert-color: #bd3a18;
+  --text-color: #f4f4f4;
+}
+
+body {
+  background-color: var(--primary-color);
+  color: var(--text-color);
 }
 </style>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,95 +1,175 @@
 <template>
-  <div>
-    <section>
-      <h1>Esperanos muy pronto...</h1>
-      <h2>Podcast</h2>
+  <div class="clock-container">
+    <section class="text-header">
+      <h1 class="text-comming">Esperanos muy pronto...</h1>
+      <h2 class="podcast-title">Podcast</h2>
     </section>
-    <div>
+    <section class="clock">
       <div>
-        <p>{{displayDays}}</p>
-        <p>dias</p>
+        <p class="clock-number">{{ displayDays }}</p>
+        <p class="clock-text">dias</p>
       </div>
-      <span>:</span>
+      <span class="double-dot">:</span>
       <div>
-        <p>{{displayHours}}</p>
-        <p>horas</p>
+        <p class="clock-number">{{ displayHours }}</p>
+        <p class="clock-text">horas</p>
       </div>
-      <span>:</span>
+      <span class="double-dot">:</span>
       <div>
-        <p>{{displayMinutes}}</p>
-        <p>minutos</p>
+        <p class="clock-number">{{ displayMinutes }}</p>
+        <p class="clock-text">minutos</p>
       </div>
-      <span>:</span>
+      <span class="double-dot">:</span>
       <div>
-        <p>{{displaySeconds}}</p>
-        <p>segundos</p>
+        <p class="clock-number">{{ displaySeconds }}</p>
+        <p class="clock-text">segundos</p>
       </div>
-    </div>
+    </section>
+    <section class="analog-clock-container">
+      <div class="analog-clock">
+        <p class="letras">Relog</p>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Counter',
-    data() {
-      return {
-        displayDays: 100,
-        displayHours: 10,
-        displayMinutes: 10,
-        displaySeconds: 10,
-      }
+export default {
+  name: "Counter",
+  data() {
+    return {
+      displayDays: 100,
+      displayHours: 10,
+      displayMinutes: 10,
+      displaySeconds: 10,
+    };
+  },
+  computed: {
+    seconds() {
+      return 1000
     },
-    // computed: {
-    //   seconds() {
-    //     return 1000
-    //   },
-    //   minutes() {
-    //     return this.seconds * 60
-    //   },
-    //   hours() {
-    //     return this.minutes * 60
-    //   },
-    //   days() {
-    //     return this.hours * 24
-    //   }
-    // },
-    // mounted () {
-    //   this.showRemaining();
-    // },
-    // methods: {
-    //   formatNum(num) {
-    //     return num < 10 ? '0' + num : num
-    //   }, 
-    //   showRemaining() {
-    //     const timer = setInterval(() => {
-    //       const now = new Date()
-    //       const end = new Date(2021, 5, 7, 10, 10, 10, 10)
-    //       const distance = end.getTime() - now.getTime()
+    minutes() {
+      return this.seconds * 60
+    },
+    hours() {
+      return this.minutes * 60
+    },
+    days() {
+      return this.hours * 24
+    }
+  },
+  mounted () {
+    this.showRemaining();
+  },
+  methods: {
+    formatNum(num) {
+      return num < 10 ? '0' + num : num
+    },
+    showRemaining() {
+      const timer = setInterval(() => {
+        const now = new Date()
+        const end = new Date(2021, 5, 7, 10, 10, 10, 10)
+        const distance = end.getTime() - now.getTime()
 
-    //       if (distance < 0) {
-    //         clearInterval(timer)
-    //         return
-    //       }
+        if (distance < 0) {
+          clearInterval(timer)
+          return
+        }
 
-    //       const daysRes = Math.floor((distance / this.days))
-    //       const hoursRes = Math.floor((distance % this.days) / this.hours)
-    //       const minutesRes = Math.floor((distance % this.hours) / this.minutes)
-    //       const secondsRes = Math.floor((distance % this.minutes) / this.seconds)
+        const daysRes = Math.floor((distance / this.days))
+        const hoursRes = Math.floor((distance % this.days) / this.hours)
+        const minutesRes = Math.floor((distance % this.hours) / this.minutes)
+        const secondsRes = Math.floor((distance % this.minutes) / this.seconds)
 
-    //       // this.displayMinutes = minutesRes < 10 ? '0' + minutesRes : minutesRes
-    //       // this.displaySeconds = secondsRes < 10 ? '0' + secondsRes : secondsRes
-    //       // this.displayHours = hoursRes < 10 ? '0' + hoursRes : hoursRes
-    //       // this.displayDays = daysRes < 10 ? '0' + daysRes : daysRes 
-    //       this.displayMinutes = this.formatNum(minutesRes)
-    //       this.displaySeconds = this.formatNum(secondsRes)
-    //       this.displayHours = this.formatNum(hoursRes)
-    //       this.displayDays = this.formatNum(daysRes)
-    //     }, 1000)
-    //   }
-    // },
-  }
+        // this.displayMinutes = minutesRes < 10 ? '0' + minutesRes : minutesRes
+        // this.displaySeconds = secondsRes < 10 ? '0' + secondsRes : secondsRes
+        // this.displayHours = hoursRes < 10 ? '0' + hoursRes : hoursRes
+        // this.displayDays = daysRes < 10 ? '0' + daysRes : daysRes
+        this.displayMinutes = this.formatNum(minutesRes)
+        this.displaySeconds = this.formatNum(secondsRes)
+        this.displayHours = this.formatNum(hoursRes)
+        this.displayDays = this.formatNum(daysRes)
+      }, 1000)
+    }
+  },
+};
 </script>
 
 <style scoped>
+p {
+  margin: 0;
+  text-align: center;
+}
 
+.clock-container {
+  margin-top: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.text-header {
+  width: 100%;
+  text-align: center;
+}
+
+.text-comming {
+  font-size: 3.5rem;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+
+.podcast-title {
+  font-size: 3rem;
+  margin-top: 0;
+  color: var(--action-color);
+}
+
+.clock {
+  width: 35%;
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.clock-number {
+  font-size: 2.8rem;
+}
+
+.clock-text {
+  font-size: 1.5rem;
+}
+
+.double-dot {
+  font-size: 2.5rem;
+}
+
+.analog-clock-container {
+  width: 100%;
+  margin-top: 5rem;
+  display: flex;
+  justify-content: center;
+}
+
+.analog-clock {
+  width: 150px;
+  height: 150px;
+  background-color: var(--text-color);
+  border-radius: 50%;
+  transform: rotateY(0deg);
+  animation: clock 5s infinite;
+  animation-direction: alternate;
+}
+
+.letras {
+  color: var(--alert-color);
+}
+
+@keyframes clock {
+  0% {
+    transform: rotateY(0);
+  }
+  100% {
+    transform: rotateY(360deg);
+  }
+}
 </style>
